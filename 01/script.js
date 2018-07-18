1a377a72394df49c03ab76f137e002566461f4b0
basbelmar <galvanize.bas@gmail.com>
2018-07-17 21:06:12 -0400

    ejercicio1 avance ejercicio2

---
+++ 01/script.js
@@ -0,0 +1,11 @@
+
+		$('document').ready(function(){
+			$("img").on('click' , function() {
+				alert($(this).attr('alt'));
+			});
+		});		
+
+
+
+
+
---
+++ 02/script.js
@@ -0,0 +1,17 @@
+/*	$(document).on("contextmenu",function(e){
+		e.preventDefault();
+
+
+document.getElementsByTagName('');
+ 
+document.getElementsByTagName('');
+    document.getElementsByTagName('elemento')[índice_del_elemento]; 
+
+
+$('document').ready(function(){
+   document.getElementsByTagName('div')[1].getElementsByTagName('li');
+   console.log ();
+});*/
+
+
+alert ('hola')
